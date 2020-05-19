09cf4975fd901ee233ecc9ab5116c4c288584ed0
Keon Hayes <khayes25@student.ccp.edu>
2020-05-19 12:52:37 -0400

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk()
+root.title("Simple Calculator")
+
+e = Entry(root, width=35, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+#e.insert(0, "Enter Your Name")
+
+button_1 = Button(root, text="1")
+
+def myClick():
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
+
+myButton = Button(root, text="Enter Your Stock Quote", pady=50, command=myClick, fg="blue", bg="#000000")
+
+root.mainloop()
